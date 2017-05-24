03a2fb20fdc04e247e8b3cc8d886b88c87512228
xiaopotian1990 <zoulu19900206>
2017-05-24 15:36:23 +0800

    Redis Exception MSSQL

--- MyOpserver/Opserver.Core/Data/Redis/RedisModule.cs
+++ MyOpserver/Opserver.Core/Data/Redis/RedisModule.cs
@@ -29,23 +29,29 @@ namespace StackExchange.Opserver.Data.Redis
             return false;
         }
 
+        /// <summary>
+        /// 加载Redis连接
+        /// </summary>
+        /// <returns></returns>
         private static List<RedisConnectionInfo> LoadRedisConnections()
         {
             var result = new List<RedisConnectionInfo>();
+            //默认实例
             var defaultServerInstances = Current.Settings.Redis.Defaults.Instances;
+            //Servers节点的子集
             var allServerInstances = Current.Settings.Redis.AllServers.Instances;
 
             foreach (var s in Current.Settings.Redis.Servers)
             {
                 var count = result.Count;
-                // Add instances that belong to any servers
+                // Add instances that belong to any servers 为Servers实例添加子集
                 allServerInstances?.ForEach(gi => result.Add(new RedisConnectionInfo(s.Name, gi)));
 
-                // Add instances defined on this server
+                // Add instances defined on this server 添加定义在Servers里的实例
                 if (s.Instances.Count > 0)
                     s.Instances.ForEach(i => result.Add(new RedisConnectionInfo(s.Name, i)));
 
-                // If we have no instances added at this point, defaults it is!
+                // If we have no instances added at this point, defaults it is! 如果没有任何实例则添加默认的
                 if (defaultServerInstances != null && count == result.Count)
                     defaultServerInstances.ForEach(gi => result.Add(new RedisConnectionInfo(s.Name, gi)));
             }
